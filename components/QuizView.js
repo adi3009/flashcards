1346a840c86fdd9e56e3a38eb410601d5d896b1d
Aditya Godara <aditya.godara@smartebusiness.co.uk>
2021-03-06 02:01:37 +0000

    feature: restart quiz and back to deck

--- components/QuizView.js
+++ components/QuizView.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import CardView from './CardView';
-import {Card, ProgressBar, Surface, Text, Title} from 'react-native-paper';
+import {Button, Card, Colors, ProgressBar, Surface, Text, Title} from 'react-native-paper';
 import {StyleSheet} from 'react-native';
 import {getDeck} from '../utils/api';
 
-function QuizView({route}) {
+function QuizView({route, navigation}) {
 
   const {deckTitle} = route.params;
 
@@ -33,24 +33,33 @@ function QuizView({route}) {
 
   return (
     !deck ? <ProgressBar indeterminate="true"/> : (
-    !deck.questions.length ?
-      <Card style={styles.content}>
-        <Card.Content>
-          <Title>No question in the deck, can not start quiz.</Title>
-        </Card.Content>
-      </Card> : (
-        showResult ?
-          <Card style={styles.content}>
-            <Card.Content>
-              <Title>{((correctAnswers / deck.questions.length) * 100).toFixed(2)}%</Title>
-            </Card.Content>
-          </Card> :
-          <Surface>
-            <ProgressBar progress={(questionId / deck.questions.length)}/>
-            <Text style={styles.txt}>{questionId + 1} / {deck.questions.length}</Text>
-            <CardView question={deck.questions[questionId]} onAnswer={onAnswer} />
-          </Surface>
-      ))
+      !deck.questions.length ?
+        <Card style={styles.content}>
+          <Card.Content>
+            <Title>No question in the deck, can not start quiz.</Title>
+          </Card.Content>
+        </Card> : (
+          showResult ?
+            <Card style={styles.content}>
+              <Card.Content>
+                <Title>{((correctAnswers / deck.questions.length) * 100).toFixed(2)}%</Title>
+              </Card.Content>
+              <Card.Actions>
+                <Button mode="contained" icon="restart" style={styles.btn} onPress={() => {
+                  setShowResult(false);
+                  setCorrectAnswers(0);
+                  setQuestionId(0);
+                }}>Restart Quiz</Button>
+                <Button mode="contained" icon="undo" color={Colors.green600} onPress={() => navigation.goBack()}
+                        style={styles.btn}>Back to Deck</Button>
+              </Card.Actions>
+            </Card> :
+            <Surface>
+              <ProgressBar progress={(questionId / deck.questions.length)}/>
+              <Text style={styles.txt}>{questionId + 1} / {deck.questions.length}</Text>
+              <CardView question={deck.questions[questionId]} onAnswer={onAnswer}/>
+            </Surface>
+        ))
   );
 }
 
@@ -61,7 +70,11 @@ const styles = StyleSheet.create({
   },
   txt: {
     padding: 8
-  }
+  },
+  btn: {
+    margin: 8,
+    padding: 8
+  },
 });
 
 export default QuizView;
